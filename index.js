2a4c88b25b739f9364a2eed47726722e0f84d05a
gautamkandpal <gkandpal33@gmail.com>
2025-08-26 17:45:03 +0530

    feat: adding server configuration

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+
+// Create an MCP server
+const server = new McpServer({
+    name: "demo-server",
+    version: "1.0.0"
+});
